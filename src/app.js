5fb33e7e383bbbafe283fa382858ece3475c7f60
mykola <nehrych@outlook.com>
2022-08-02 20:09:54 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from "./styles.module.scss"
+
+const App = () => {
+    return (
+        <div className={style.wrap}>
+            App is work
+        </div>
+    );
+};
+
+export default App;
